02935a2940e671868ff4232d02436f8a83af5586
Avinash <avi190824@gmail.com>
2021-10-05 14:58:53 +0530

    Login and Register completed

---
+++ app/src/main/java/com/example/parklot/REGISTERPAGE.java
@@ -0,0 +1,14 @@
+package com.example.parklot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class REGISTERPAGE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerpage);
+    }
+}
